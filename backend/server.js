9e7955e6bd47e73fe9b06264fe02c355353996f3
shrikant-kushwah <shrikant20052001@gmail.com>
2024-10-12 02:17:03 +0530

    updated server file

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,5 @@
-const app = require("./app");
-const connectDB = require("./config/db");
-
-require("dotenv").config();
-
+const app = require('./app');
 const PORT = process.env.PORT || 5000;
-
-connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

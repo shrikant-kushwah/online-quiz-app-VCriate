1fb0851a5f9b2bfa51554de3d74cee55ac67ae9b
shrikant-kushwah <shrikant20052001@gmail.com>
2024-10-12 02:02:48 +0530

    updated app file

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const quizRoutes = require("./routes/quizRoutes");
 
 const app = express();
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
+app.use("/api/quizzes", quizRoutes);
 
 module.exports = app;

de533c79d1d9bb10a55d23a36a99a1c522cea144
shrikant-kushwah <shrikant20052001@gmail.com>
2024-10-12 02:13:31 +0530

    updated app file & install cors

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,14 @@ const express = require("express");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const quizRoutes = require("./routes/quizRoutes");
+const cors = require('cors');
+
+require('dotenv').config();
+
+connectDB();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
